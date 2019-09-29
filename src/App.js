fc4a9552d59f85fde795b841bf5d0f9d61366feb
Davi Silva <davi.cunha.silva.ca@gmail.com>
2019-09-29 03:27:58 -0300

    Frontend login auth working alongside the backend using passportjs

--- src/App.js
+++ src/App.js
@@ -6,28 +6,34 @@ import "./App.css";
 import Navbar from "./components/layouts/Navbar.component";
 import Footer from "./components/layouts/Footer.component";
 
-import Admin from "./components/pages/admin.component";
-import Homepage from "./components/pages/homepage.component";
-import Login from "./components/pages/login.component";
-import Register from "./components/pages/register.component";
-import Dashboard from "./components/pages/dashboard.component";
-import BlogHome from "./components/pages/blog-home.component";
-import About from "./components/pages/about.component";
+import Admin from "./pages/admin.component";
+import Homepage from "./pages/homepage.component";
+import Login from "./pages/login.component";
+import Register from "./pages/register.component";
+import Dashboard from "./pages/dashboard.component";
+import BlogHome from "./pages/blog-home.component";
+import About from "./pages/about.component";
+import Profile from "./pages/Profile";
+import UserProvider from "./contexts/UserProvider";
+import history from "./history";
 
 function App() {
-	return (
-		<Router>
-			<Navbar />
-			<Route path="/" exact component={Homepage} />
-			<Route path="/admin" component={Admin} />
-			<Route path="/dashboard" component={Dashboard} />
-			<Route path="/login" component={Login} />
-			<Route path="/register" component={Register} />
-			<Route path="/blog" component={BlogHome} />
-			<Route path="/about" component={About} />
-			<Footer />
-		</Router>
-	);
+  return (
+    <Router history={history}>
+      <Navbar />
+      <Route path="/" exact component={Homepage} />
+      <Route path="/admin" component={Admin} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/blog" component={BlogHome} />
+      <Route path="/about" component={About} />
+      <UserProvider>
+        <Route path="/profile" component={Profile} />
+      </UserProvider>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/contexts/UserProvider.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState, useEffect } from "react";
+const context = createContext(null);
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:5000/user")
+      .then(res => res.json())
+      .then(res => {
+        console.log("res:", res);
+        setUser(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return <context.Provider value={user}>{children}</context.Provider>;
+};
+
+UserProvider.context = context;
+
+export default UserProvider;
