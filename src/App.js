8b0353041eaeff12c091624fac7fbec5b4707d73
Davi-Silva <davi.cunha.silva.ca@gmail.com>
2019-10-02 16:48:40 -0300

    filename renamed.

--- src/App.js
+++ src/App.js
@@ -6,34 +6,34 @@ import "./App.css";
 import Navbar from "./components/layouts/Navbar.component";
 import Footer from "./components/layouts/Footer.component";
 
-import Admin from "./pages/admin.component";
-import Homepage from "./pages/homepage.component";
-import Login from "./pages/login.component";
-import Register from "./pages/register.component";
-import Dashboard from "./pages/dashboard.component";
-import BlogHome from "./pages/blog-home.component";
-import About from "./pages/about.component";
+import Admin from "./pages/Admin";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import BlogHome from "./pages/BlogHome";
+import About from "./pages/About";
 import Profile from "./pages/Profile";
 import UserProvider from "./contexts/UserProvider";
 import history from "./history";
 
 function App() {
-  return (
-    <Router history={history}>
-      <UserProvider>
-        <Navbar />
-        <Route path="/" exact component={Homepage} />
-        <Route path="/admin" component={Admin} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/blog" component={BlogHome} />
-        <Route path="/about" component={About} />
-        <Route path="/profile" component={Profile} />
-      </UserProvider>
-      <Footer />
-    </Router>
-  );
+	return (
+		<Router history={history}>
+			<UserProvider>
+				<Navbar />
+				<Route path="/" exact component={Homepage} />
+				<Route path="/admin" component={Admin} />
+				<Route path="/dashboard" component={Dashboard} />
+				<Route path="/login" component={Login} />
+				<Route path="/register" component={Register} />
+				<Route path="/blog" component={BlogHome} />
+				<Route path="/about" component={About} />
+				<Route path="/profile" component={Profile} />
+			</UserProvider>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
