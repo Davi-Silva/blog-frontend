6c649ed7464e984f238376eb4f1e4c9a3acda2c0
Davi-Silva <davi.cunha.silva.ca@gmail.com>
2019-10-04 11:57:44 -0300

    .

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 
 class Course extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <h1>Course</h1>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-12">
+						<h1>Course Page</h1>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Course;
