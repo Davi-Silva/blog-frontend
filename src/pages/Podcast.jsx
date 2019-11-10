f25a5f12b34ea45691e35941a95a27e38dee5aaf
Davi Silva <davi.cunha.silva.ca@gmail.com>
2019-11-10 13:53:23 -0300

    audio player is pa been mounted but is not playing the audio source.

--- src/pages/Podcast.jsx
+++ src/pages/Podcast.jsx
@@ -102,8 +102,8 @@ export default class Podcast extends Component {
     const { match } = this.props;
     const { slug } = match.params;
     this.response = await fetch(
-      // `https://course-backend.herokuapp.com/podcasts/get/${slug}`,
-      `http://localhost:5000/podcasts/get/${slug}`,
+      `https://course-backend.herokuapp.com/podcasts/get/${slug}`,
+      // `http://localhost:5000/podcasts/get/${slug}`,
       {
         method: 'GET',
         mode: 'cors',
