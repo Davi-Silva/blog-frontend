5de75ed82f464d3bc38460bfe53e31b3fdaca4a7
Davi Silva <davi.cunha.silva.ca@gmail.com>
2019-12-27 01:07:38 -0300

    .

--- src/pages/RegisterAdmin.jsx
+++ src/pages/RegisterAdmin.jsx
@@ -1,25 +1,190 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   FaUsersCog,
+  FaUserPlus,
 } from 'react-icons/fa';
 
+import {
+  TitleDiv,
+  Form,
+  Warning,
+} from '../styled-components/register-admin.styled-components';
+
 const RegisterAdmin = () => {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [error, setError] = useState(false);
+  const [adminUser, setAdminUser] = useState('');
+  const [adminPassword, setAdminPassword] = useState('');
+  const [adminPassword2, setAdminPassword2] = useState('');
+
   const handleRegistration = async () => {
 
   };
 
-  const verifyRightAnswer = async () => {
-
+  const LoginSU = async (userInfo) => {
+    console.log('userInfo:', userInfo);
+    const response = await fetch('http://localhost:5000/admin/user/verify/su',
+      {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userInfo),
+      });
+    const data = await response.json();
+    return data;
   };
+
+  const handleUser = (e) => {
+    setUser(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleAdminUser = (e) => {
+    setAdminUser(e.target.value);
+  };
+
+  const handleAdminPassword = (e) => {
+    setAdminPassword(e.target.value);
+  };
+
+  const handleAdminPassword2 = (e) => {
+    setAdminPassword2(e.target.value);
+  };
+
+  const handleLoginSU = async (e) => {
+    e.preventDefault();
+    const userInfo = {
+      user,
+      password,
+    };
+    const res = await LoginSU(userInfo);
+
+    setIsAdmin(res.isAdmin);
+    if (!isAdmin) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
+
+
+  let registerAdmin;
+  let warning;
+
+  if (error) {
+    warning = (
+      <>
+        <br />
+        <Warning>
+          Not Admin
+        </Warning>
+      </>
+    );
+  } else {
+    warning = (
+      <>
+
+      </>
+    );
+  }
+
+  if (isAdmin) {
+    registerAdmin = (
+      <>
+        <div className="col-12">
+          <TitleDiv>
+            <FaUserPlus />
+            <h1>Register new admin</h1>
+          </TitleDiv>
+          <Form method="post" onSubmit={handleLoginSU}>
+            <input
+              type="text"
+              name="user"
+              id="user"
+              value={adminUser}
+              placeholder="User"
+              autoComplete="off"
+              onChange={handleAdminUser}
+            />
+            <br />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={adminPassword}
+              placeholder="Password"
+              onChange={handleAdminPassword}
+            />
+            <br />
+            <input
+              type="password2"
+              name="password2"
+              id="password2"
+              value={adminPassword2}
+              placeholder="Confirm Password"
+              onChange={handleAdminPassword2}
+            />
+            <br />
+            <button type="submit">
+              Register
+            </button>
+          </Form>
+        </div>
+      </>
+    );
+  } else {
+    registerAdmin = (
+      <>
+        <div className="col-12">
+          <TitleDiv>
+            <FaUsersCog />
+            <h1>Super User</h1>
+          </TitleDiv>
+          <Form method="post" onSubmit={handleLoginSU}>
+            <input
+              type="text"
+              name="user"
+              id="user"
+              value={user}
+              placeholder="User"
+              autoComplete="off"
+              onChange={handleUser}
+            />
+            <br />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              placeholder="Password"
+              onChange={handlePassword}
+            />
+            {warning}
+
+            <button type="submit">
+              Login
+            </button>
+          </Form>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
       <div className="container">
         <div className="row">
-          <div className="col-12">
-            <FaUsersCog />
-          </div>
+          {registerAdmin}
         </div>
       </div>
     </>
---
+++ src/styled-components/register-admin.styled-components.jsx
@@ -0,0 +1,73 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+
+export const TitleDiv = styled.div`
+  margin: 15px auto 0 auto; 
+  display: table;
+  svg {
+    margin: 0 auto;
+    display: table;
+    font-size: 25px;
+    color: #333;
+  }
+  h1 {
+    margin: 0 auto;
+    display: table;
+    font-size: 18px;
+    color: #333;
+  }
+`;
+
+export const Form = styled.form`
+  margin: 20px auto 10px auto;
+  display: table;
+  input {
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    margin: 0 0 5px 0;
+    border-radius: 5px;
+    color: #333;
+    padding: 5px 8px;
+    transition: .2s all ease-in-out;
+    &:focus {
+      outline: none;
+      border: 1px solid #0058e4;
+    }
+    &::placeholder {
+      color: #333;
+    }
+  }
+  button {
+    margin: 0 auto;
+    display: table;
+    background-color: transparent;
+    padding: 5px 8px;
+    border: none;
+    border-radius: 5px;
+    color: #999;
+    font-weight: 900;
+    letter-spacing: 1px;
+    transition: .2s all ease-in-out;
+    &:hover {
+      background-color: #f2f3f5;
+    }
+    &:focus {
+      outline: none;
+    }
+    &:active {
+      background-color: #dedede;
+      color: #888;
+    }
+  }
+`;
+
+export const Warning = styled.div`
+  border: 1px solid #d42626;
+  background-color: #d426260f;
+  display: table;
+  margin: 5px auto;
+  border-radius: 5px;
+  padding: 5px 8px;
+  color: #d42626;
+  font-size: 13px;
+  letter-spacing: 1px;
+`;
