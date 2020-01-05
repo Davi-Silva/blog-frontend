6f6649054b79db41a7a7cd6650b8e9d6781985aa
Davi Silva <davi.cunha.silva.ca@gmail.com>
2020-01-04 22:08:16 -0300

    conficts solved

--- src/pages/PublicProfile.jsx
+++ src/pages/PublicProfile.jsx
@@ -22,20 +22,6 @@ const PublicProfile = (props) => {
   const userInfo = useContext(UserProvider.context);
   console.log('userInfo:', userInfo);
 
-  const getPublicUser = async (user) => {
-    const response = await fetch(`http://localhost:5000/user/public-profile/${user}`, {
-      method: 'GET',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const data = await response.json();
-    return data;
-  };
-
   useEffect(() => {
     const {
       user,
