608626c9f85dcc912b9bbb15753bcc0eb7e6755d
Davi Silva <davi.cunha.silva.ca@gmail.com>
2020-01-06 20:02:51 -0300

    .

--- src/styled-components/advertisements.styled-components.jsx
+++ src/styled-components/advertisements.styled-components.jsx
@@ -7,7 +7,8 @@ export const AdSquare = styled.div`
 	box-shadow: 0px 1px 2px rgba(0,0,0,0.2);
 	border: 1px solid rgba(0, 0, 0, 0.15);
 	width: 100%;
-	height: 300px;
+	height: 200px;
+	margin-bottom: 20px;
 	/* position: fixed; */
 	@media (max-width: 768px) {
 		min-height: 200px;
