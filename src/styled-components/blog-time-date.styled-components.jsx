2f499d7cf3ad2f16c43b7199ce4d96d9d8d479ca
Davi Silva <davi.cunha.silva.ca@gmail.com>
2020-01-06 14:04:00 -0300

    ;

--- src/styled-components/blog-time-date.styled-components.jsx
+++ src/styled-components/blog-time-date.styled-components.jsx
@@ -9,22 +9,18 @@ export const Wrapper = styled.div`
 export const DayMonthYear = styled.span`
 	margin: 15px 0;
 	color: #333;
-	font-size: 20px;
+	font-size: 18px;
 	font-weight: 900;
 	float: right;
 	@media (max-width: 1199px) {
-		font-size: 18px;
+		font-size: 16px;
 	}
 	@media (max-width: 991px) {
-		font-size: 17px;
+		font-size: 15px;
 		margin: 10px 0;
 	}
 	@media (max-width: 768px) {
-		font-size: 16px;
-		margin: 8px 0;
-	}
-	@media (max-width: 576px) {
-		font-size: 15px;
+		font-size: 14px;
 		margin: 8px 0;
 	}
 `;
