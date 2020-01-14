eeadc29ad625d6ddcba1fd56bf5c25cd1f4bea15
Davi Silva <davi.cunha.silva.ca@gmail.com>
2020-01-14 14:52:10 -0300

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,5 @@
 const proxy = require('http-proxy-middleware');
 
-<<<<<<< HEAD
-module.exports = function (app) {
-  app.use(proxy('/auth', { target: 'https://cryptic-activist-backend.herokuapp.com/' }));
-=======
 module.exports = function(app) {
   app.use(proxy("/auth", { target: "https://cryptic-activist-backend.herokuapp.com/" }));
->>>>>>> feature
 };
