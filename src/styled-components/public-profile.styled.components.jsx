6a192bfb2ae93be1d83fd93e532b643ea943526d
Davi Silva <davi.cunha.silva.ca@gmail.com>
2020-01-23 16:36:47 -0300

    .

--- src/styled-components/public-profile.styled.components.jsx
+++ src/styled-components/public-profile.styled.components.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 import { rotate } from './animations';
 
