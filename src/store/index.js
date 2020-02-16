06642e9ef0cdbcfc4ff83ef018a751e9c5d4f877
Davi Silva <davi.cunha.silva.ca@gmail.com>
2020-02-16 00:52:28 -0300

    redux dev tools removed.

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-underscore-dangle */
-import { createStore, applyMiddleware, compose } from 'redux';
-import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
 
-import rootReducer from './reducers';
-import rootSaga from './sagas';
+import rootReducer from "./reducers";
+import rootSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+    applyMiddleware(sagaMiddleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 sagaMiddleware.run(rootSaga);
